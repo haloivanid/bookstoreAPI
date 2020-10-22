0fad00540bb880d6175b8ec319c12d3190d2cadf
Ivan Azis Nabawi <haloivan.idn@gmail.com>
2020-10-22 15:14:06 +0700

    solve authorization return undefined

--- middleware/authorizationMiddleware.js
+++ middleware/authorizationMiddleware.js
@@ -1,13 +1,17 @@
 const hyperId = require('hyperid')
 
 function authorization(req, res, next) {
-    // ** Bearer SGLt35GpRsyochKuWU2SIg/0
     const authorization = req.headers.authorization
-    const token = authorization.split(' ')[1]
-    const isValidToken = hyperId.decode(token)
-    console.log(isValidToken);
-    if (isValidToken) {
-        next()
+    if (authorization) {
+        token = authorization.split(' ')[1]
+        const isValidToken = hyperId.decode(token)
+        console.log(isValidToken);
+        if (isValidToken) {
+            next()
+        }
+        else {
+            res.status(401).send('Unauthorized')
+        }
     }
     else {
         res.status(401).send('Unauthorized')
